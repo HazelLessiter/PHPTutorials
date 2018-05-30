ccc8d2e591ceb43766f1f814e2d6ab48a1fb38a6
Hazel Lessiter <39559927+HazelLessiter@users.noreply.github.com>
2018-05-30 19:07:44 +0100

    Add files via upload

---
+++ alternativephptutorial_2.php
@@ -0,0 +1,5 @@
+<?php echo 'First is the worst';
+	echo 'Second is the best';
+	echo 'Third is better then all the rest';
+	echo 'Fourth, of course';
+	echo 'Fifth is- I know this is not right but give me a break it has been ten years since I heard the rythme';
---
+++ phptutorial_2.php
@@ -0,0 +1,4 @@
+<?php echo 'This will get printed first';
+	echo 'This will get printed second';
+	echo 'This will get printed third';
+	echo 'And this one does not even get a podium finish. Fail.';
